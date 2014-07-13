43c031415fbaa4de962ba21f531a5d06bdccd738
Jan Tojnar <jtojnar@gmail.com>
2014-07-13 09:38:03 +0200

    userLink macro

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -3,6 +3,7 @@
 namespace App\Presenters;
 
 use Nette,
+	Nette\Utils\Html,
 	App\Model;
 
 
@@ -14,5 +15,13 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter {
 		$vp = new \VisualPaginator($this, $name);
 		$vp->getPaginator()->itemsPerPage = 10;
 		return $vp;
+	}
+
+	protected function createTemplate($class=null) {
+		$template = parent::createTemplate($class);
+		$template->registerHelper('userLink', function($user) use ($template) {
+			return Html::el('a', $user->username)->href($template->presenter->link('profile:show', $user->id));
+		});
+		return $template;
 	}
 }
