0083d97ecc75b710483e4ee714d525b65ee8c62e
Jan Tojnar <jtojnar@gmail.com>
2014-07-13 08:24:23 +0200

    added basic profiles

---
+++ app/presenters/ProfilePresenter.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette, App\Model;
+
+
+/**
+ * Profile presenter.
+ */
+class ProfilePresenter extends BasePresenter {
+	/** @var Nette\Database\Context @inject */
+	public $database;
+
+	public function renderShow($id) {
+		$profile = $this->database->table('user')->get($id);
+		if(!$profile) {
+			$this->error('Uživatel nenalezen');
+		}
+		if(file_exists($this->context->parameters['avatarStorage'] . '/' . $profile->id . 'm.png')) {
+			$this->template->avatar = str_replace('♥basePath♥', $this->context->httpRequest->url->baseUrl, $this->context->parameters['avatarStoragePublic']) . '/' . $profile->id . 'm.png';
+		}
+
+		$this->template->isMe = $this->user->loggedIn && $this->user->identity->id === $profile->id;
+		$this->template->ipAddress = $this->context->httpRequest->remoteAddress;
+		$this->template->profile = $profile;
+	}
+}
