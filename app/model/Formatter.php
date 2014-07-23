f2caae70f69a85d07d2d868a4222833f37344811
Jan Tojnar <jtojnar@gmail.com>
2014-07-23 23:54:19 +0200

    implemented oEmbed for Formatter

--- app/model/Formatter.php
+++ app/model/Formatter.php
@@ -2,18 +2,43 @@
 
 namespace App\Model;
 
+use Alb\OEmbed;
+
 class Formatter extends \Nette\Object {
+	public static $OEMBED_WHITELIST = ['www.youtube.com'];
+
 	/** @var \Parsedown */
 	public $parsedown;
+
 	/** @var \HTMLPurifier */
 	public $purifier;
 
-	public function __construct(\Parsedown $parsedown, \HTMLPurifier $purifier) {
+	/** @var OEmbed\Simple */
+	public $oembed;
+
+	private $replacements = [];
+
+	public function __construct(\Parsedown $parsedown, \HTMLPurifier $purifier, OEmbed\Simple $oembed) {
 		$this->parsedown = $parsedown;
 		$this->purifier = $purifier;
+		$this->oembed = $oembed;
 	}
 
 	public function format($text) {
+		$alpha = "a-z\x80-\xFF";
+		$domain = "[0-9$alpha](?:[-0-9$alpha]{0,61}[0-9$alpha])?";
+		$topDomain = "[$alpha][-0-9$alpha]{0,17}[$alpha]";
+		if(preg_match_all("(^https?://((?:$domain\\.)*$topDomain|\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|\\[[0-9a-f:]{3,39}\\])(:\\d{1,5})?(/\\S*)?$)im", $text, $matches, PREG_SET_ORDER)) {
+			foreach($matches as $match) {
+				if(in_array($match[1], Formatter::$OEMBED_WHITELIST) && !isset($this->replacements[$match[0]])) {
+					try {
+						$this->replacements[$match[0]] = '<figure>' . $this->oembed->request($match[0])->getHtml() . '</figure>';
+					} catch(\Exception $e) {}
+				}
+			}
+		}
+		$text = strTr($text, $this->replacements);
+	
 		$text = $this->parsedown->parse($text);
 
 		$text = $this->purifier->purify($text);
