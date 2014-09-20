d1a87ced8e7cddc917b17323682160bff2caf4fa
Jan Tojnar <jtojnar@gmail.com>
2014-09-20 19:12:01 +0200

    added diff, fixes #15

--- app/presenters/RevisionPresenter.php
+++ app/presenters/RevisionPresenter.php
@@ -2,7 +2,7 @@
 namespace App\Presenters;
 
 use Nette;
-use Nextras\Forms\Rendering;
+use Caxy\HtmlDiff\HtmlDiff;
 
 class RevisionPresenter extends BasePresenter {
 	/** @var Nette\Database\Context @inject */
@@ -16,5 +16,26 @@ class RevisionPresenter extends BasePresenter {
 		$page = $revision->ref('page');
 		$this->template->page = $page;
 		$this->template->revision = $revision;
+	}
+
+	public function renderDiff($id, $and) {
+		$old = $this->database->table('page_revision')->get($id);
+		if(!$old) {
+			$this->error('Revize nenalezena.');
+		}
+		$new = $this->database->table('page_revision')->get($and);
+		if(!$new) {
+			$this->error('Revize nenalezena.');
+		}
+
+		$differ = new HtmlDiff($old->content, $new->content);
+		$differ->build();
+		$diff = $differ->getDifference();
+
+		$page = $old->ref('page');
+		$this->template->page = $page;
+		$this->template->diff = $diff;
+		$this->template->old = $old;
+		$this->template->new = $new;
 	}
 }
