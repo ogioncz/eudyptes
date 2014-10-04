0b03001ad44dc6a376db59b683922fa10ddc51da
Jan Tojnar <jtojnar@gmail.com>
2014-10-04 21:51:06 +0200

    fixed mails

--- app/model/orm/mail/MailMapper.php
+++ app/model/orm/mail/MailMapper.php
@@ -9,6 +9,8 @@ class MailMapper extends BaseMapper {
 	protected function createStorageReflection() {
 		$reflection = parent::createStorageReflection();
 		$reflection->addMapping('reaction', 'reaction');
+		$reflection->addMapping('recipient', 'recipient');
+		$reflection->addMapping('sender', 'sender');
 		return $reflection;
 	}
 }
