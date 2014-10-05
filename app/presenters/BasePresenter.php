d22e09117135fc91b49ae382c415963bd2f50a00
Jan Tojnar <jtojnar@gmail.com>
2014-10-05 18:12:17 +0200

    show upcoming meetings

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -9,6 +9,9 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter {
 	/** @var App\Model\UserRepository @inject */
 	public $users;
 
+	/** @var App\Model\MeetingRepository @inject */
+	public $meetings;
+
 	protected function createComponentPaginator($name) {
 		$vp = new \VisualPaginator($this, $name);
 		$vp->getPaginator()->itemsPerPage = 10;
@@ -24,7 +27,9 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter {
 	public function beforeRender() {
 		parent::beforeRender();
 		if($this->user->loggedIn) {
-			$this->template->unreadMails = $this->users->getById($this->user->identity->id)->receivedMail->get()->findBy(['read' => false])->count();
+			$user = $this->users->getById($this->user->identity->id);
+			$this->template->unreadMails = $user->receivedMail->get()->findBy(['read' => false])->count();
+			$this->template->upcomingMeetings = $this->meetings->findUpcoming()->count();
 		}
 	}
 }
