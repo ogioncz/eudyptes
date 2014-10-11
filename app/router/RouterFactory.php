e2473fbc7cd695747fa54de54e93f319f9846baf
Jan Tojnar <jtojnar@gmail.com>
2014-10-11 22:06:49 +0200

    added legacy redirect, fixes #36

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -13,6 +13,22 @@ class RouterFactory {
 	 */
 	public function createRouter() 	{
 		$router = new RouteList();
+		$router[] = new Route('post/<id \d+>', 'Post:show', Route::ONE_WAY);
+		$router[] = new Route('post/<id \d+>/edit', 'Post:edit', Route::ONE_WAY);
+		$router[] = new Route('<slug .+>.html', 'Page:show', Route::ONE_WAY);
+		$router[] = new Route('user/login', 'Sign:in', Route::ONE_WAY);
+		$router[] = new Route('user/logout', 'Sign:out', Route::ONE_WAY);
+		$router[] = new Route('user/register', 'Profile:create', Route::ONE_WAY);
+		$router[] = new Route('user/edit', 'Profile:edit', Route::ONE_WAY);
+		$router[] = new Route('profile/<id \d+>', 'Profile:show', Route::ONE_WAY);
+		$router[] = new Route('profile', 'Profile:list', Route::ONE_WAY);
+		$router[] = new Route('meeting', 'Meeting:list', Route::ONE_WAY);
+		$router[] = new Route('meeting/new', 'Meeting:create', Route::ONE_WAY);
+		$router[] = new Route('meeting/<id \d+>/<action (edit|delete)>', ['presenter' => 'Meeting'], Route::ONE_WAY);
+		$router[] = new Route('rss.xml', 'Post:rss', Route::ONE_WAY);
+		$router[] = new Route('mail', 'Mail:list', Route::ONE_WAY);
+		$router[] = new Route('mail/sent', ['presenter' => 'Mail', 'action' => 'list', 'sent' => true], Route::ONE_WAY);
+		$router[] = new Route('mail/<id \d+>', 'Mail:show', Route::ONE_WAY);
 		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
 		return $router;
 	}
