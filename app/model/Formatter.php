17f2516edf52b1aaccbc6a994652b79c37c03e46
Jan Tojnar <jtojnar@gmail.com>
2014-12-02 20:41:11 +0100

    fixed null oembed request

--- app/model/Formatter.php
+++ app/model/Formatter.php
@@ -34,7 +34,10 @@ class Formatter extends Nette\Object {
 			foreach($matches as $match) {
 				if(in_array($match[1], Formatter::$OEMBED_WHITELIST) && !isset($this->replacements[$match[0]])) {
 					try {
-						$this->replacements[$match[0]] = '<figure class="rwd-media rwd-ratio-16-9">' . $this->oembed->request($match[0])->getHtml() . '</figure>';
+						$request = $this->oembed->request($match[0]);
+						if($request) {
+							$this->replacements[$match[0]] = '<figure class="rwd-media rwd-ratio-16-9">' . $request->getHtml() . '</figure>';
+						}
 					} catch(\Exception $e) {
 						\Tracy\Debugger::log($e);
 					} // can’t serve, link is better than nothing so let’s leave it at that
