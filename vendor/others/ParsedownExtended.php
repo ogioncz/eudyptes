2d34f9d7a22a35917c494417b45c696c8300e995
Jan Tojnar <jtojnar@gmail.com>
2015-01-18 05:20:46 +0100

    composer: updated dependencies

--- vendor/others/ParsedownExtended.php
+++ vendor/others/ParsedownExtended.php
@@ -1,58 +1,54 @@
 <?php
 class ParsedownExtended extends ParsedownExtra {
 	function __construct() {
-		$this->BlockTypes['!'][] = 'Details';
+		$this->BlockTypes[chr(ord('¡'))][] = 'Details';
 		parent::__construct();
 	}
 
 
-	# Details
-	protected function identifyDetails($Line) {
-		if (preg_match('/^(['.$Line['text'][0].']{3,})[ ]*(.+)?[ ]*$/', $Line['text'], $matches)) {
-			$summary = array(
-				'name' => 'summary',
-				'text' => 'Pro zobrazení zápletky klikni',
-			);
+	// Details
+	protected function blockDetails($Line) {
+		if (preg_match('/^([¡]{3,})[ ]*(.+)?[ ]*$/', $Line['text'], $matches)) {
+			$Block = [
+				'char' => $Line['text'][0],
+				'markup' => ''
+			];
+
+			$Block['summary'] = 'Pro zobrazení zápletky klikni';
 
 			if (isset($matches[2])) {
-				$summary['text'] = $matches[2];
+				$Block['summary'] = $matches[2];
 			}
-			
-			$Block = [
-			'char' => $Line['text'][0],
-			'element' => [
-				'name' => 'details',
-				'handler' => 'elements',
-				'text' => [$summary, ''],
-				],
-			];
 
 			return $Block;
 		}
 	}
 
 
-	protected function addToDetails($Line, $Block) {
+	protected function blockDetailsContinue($Line, $Block) {
 		if (isset($Block['complete'])) {
 			return;
 		}
 		if (isset($Block['interrupted'])) {
-			$Block['element']['text'][1] .= "\n";
+			$Block['markup'] .= "\n";
 			unset($Block['interrupted']);
 		}
-		if (preg_match('/^'.$Block['char'].'{3,}[ ]*$/', $Line['text'])) {
-			$Block['element']['text'][1] = substr($Block['element']['text'][1], 1);
+		if (preg_match('/^!{3,}[ ]*$/', $Line['text'])) {
+			$Block['markup'] = substr($Block['markup'], 1);
 			$Block['complete'] = true;
 			return $Block;
 		}
-		$Block['element']['text'][1] .= "\n".$Line['body'];
+		$Block['markup'] .= "\n".$Line['body'];
 
 		return $Block;
 	}
 
 
-	protected function completeDetails($Block) {
-		$Block['element']['text'][1] = $this->text($Block['element']['text'][1]);
+	protected function blockDetailsComplete($Block) {
+		$Block['markup'] = '<details>' . PHP_EOL
+		. '<summary>' . $Block['summary'] . '</summary>' . PHP_EOL
+		. $this->text($Block['markup']) . PHP_EOL
+		. '</details>';
 		return $Block;
 	}
 }
