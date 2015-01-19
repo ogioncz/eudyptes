945a3004ceb8935e79752e04089130013128cf18
Jan Tojnar <jtojnar@gmail.com>
2015-01-19 04:02:28 +0100

    refactoring: separated parts of formatter to methods

--- app/model/Formatter.php
+++ app/model/Formatter.php
@@ -18,8 +18,6 @@ class Formatter extends Nette\Object {
 	/** @var OEmbed\Simple */
 	public $oembed;
 
-	private $replacements = [];
-
 	public function __construct(\Parsedown $parsedown, \HTMLPurifier $purifier, OEmbed\Simple $oembed) {
 		$this->parsedown = $parsedown;
 		$this->purifier = $purifier;
@@ -27,16 +25,31 @@ class Formatter extends Nette\Object {
 	}
 
 	public function format($text) {
+		$text = $this->replaceOembed($text);
+
+		$text = $this->replaceProps($text);
+
+		$text = $this->replaceCustomTags($text);
+
+		$text = $this->parsedown->text($text);
+
+		$text = $this->purifier->purify($text);
+
+		return ['text' => $text, 'errors' => $this->purifier->context->get('ErrorCollector')->getRaw()];
+	}
+
+	public function replaceOembed($text) {
+		$replacements = [];
 		$alpha = "a-z\x80-\xFF";
 		$domain = "[0-9$alpha](?:[-0-9$alpha]{0,61}[0-9$alpha])?";
 		$topDomain = "[$alpha][-0-9$alpha]{0,17}[$alpha]";
 		if (preg_match_all("(^https?://((?:$domain\\.)*$topDomain|\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|\\[[0-9a-f:]{3,39}\\])(:\\d{1,5})?(/\\S*)?$)im", $text, $matches, PREG_SET_ORDER)) {
 			foreach ($matches as $match) {
-				if (in_array($match[1], Formatter::$OEMBED_WHITELIST) && !isset($this->replacements[$match[0]])) {
+				if (in_array($match[1], Formatter::$OEMBED_WHITELIST) && !isset($replacements[$match[0]])) {
 					try {
 						$request = $this->oembed->request($match[0]);
 						if ($request) {
-							$this->replacements[$match[0]] = '<figure class="rwd-media rwd-ratio-16-9">' . $request->getHtml() . '</figure>';
+							$replacements[$match[0]] = '<figure class="rwd-media rwd-ratio-16-9">' . $request->getHtml() . '</figure>';
 						}
 					} catch (\Exception $e) {
 						\Tracy\Debugger::log($e);
@@ -44,8 +57,12 @@ class Formatter extends Nette\Object {
 				}
 			}
 		}
-		$text = strTr($text, $this->replacements);
+		$text = strTr($text, $replacements);
 
+		return $text;
+	}
+
+	public function replaceProps($text) {
 		$text = preg_replace('/<prop>vystavba<\/prop>/i', '<figure><img src="http://cdn.fan-club-penguin.cz/img/vystavba.gif"></figure>', $text);
 		$text = preg_replace('/<prop>fieldop<\/prop>/i', '<figure><img alt="Field-op" src="http://upload.fan-club-penguin.cz/files/system/phone-red-pulsing-big.gif"></figure>', $text);
 		$text = preg_replace('/<prop>message<\/prop>/i', '<figure><img alt="Zpráva" src="http://upload.fan-club-penguin.cz/files/system/phone-blue-pulsing-big.gif"></figure>', $text);
@@ -53,16 +70,16 @@ class Formatter extends Nette\Object {
 		$text = preg_replace('/<prop>sponsored<\/prop>/i', '<img title="Sponsorovaná párty" alt="Sponsorovaná párty" src="http://cdn.fan-club-penguin.cz/img/sponsored.png" width="14" height="14">', $text);
 		$text = preg_replace('/<prop>multiclip<\/prop>/i', '<span class="icon-random" title="Více klipů náhodně míchaných při přehrávání"></span>', $text);
 
+		return $text;
+	}
+
+	public function replaceCustomTags($text) {
 		$text = CustomTags::age($text);
 		$text = CustomTags::item($text);
 		$text = CustomTags::coins($text);
 		$text = CustomTags::music($text);
 
-		$text = $this->parsedown->text($text);
-
-		$text = $this->purifier->purify($text);
-
-		return ['text' => $text, 'errors' => $this->purifier->context->get('ErrorCollector')->getRaw()];
+		return $text;
 	}
 
 	public function formatErrors($errors) {
