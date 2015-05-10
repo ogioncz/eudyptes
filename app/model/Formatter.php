e6308b72fbdb6ffb0c40bc582d0d2e0deacb2e57
Jan Tojnar <jtojnar@gmail.com>
2015-05-10 18:39:48 +0200

    formatter: allow twitter oembed

--- app/model/Formatter.php
+++ app/model/Formatter.php
@@ -7,7 +7,7 @@ use Alb\OEmbed;
 use Nette\Utils\Html;
 
 class Formatter extends Nette\Object {
-	public static $OEMBED_WHITELIST = ['www.youtube.com', 'youtu.be', 'vimeo.com', 'soundcloud.com'];
+	public static $OEMBED_WHITELIST = ['www.youtube.com', 'youtu.be', 'vimeo.com', 'soundcloud.com', 'twitter.com'];
 
 	/** @var \Parsedown */
 	public $parsedown;
