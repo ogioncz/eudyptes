02a6b2042f891774f526f180bd289a8aa6353b29
Jan Tojnar <jtojnar@gmail.com>
2016-04-18 23:07:30 +0200

    gulpfile: formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp');
-var less = require('gulp-less'); 
+var less = require('gulp-less');
 var autoprefix = require('gulp-autoprefixer');
 var csso = require('gulp-csso');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var notify = require("gulp-notify"); 
+var notify = require("gulp-notify");
 var bower = require('gulp-bower');
 
 var config = {
 	assets: {
-		vendor: './assets/vendor' ,
+		vendor: './assets/vendor',
 		less: './assets/less',
 		javascript: './assets/javascript'
 	},
@@ -19,28 +19,28 @@ var config = {
 	}
 }
 
-gulp.task('bower', function() { 
+gulp.task('bower', function() {
 	return bower()
-	.pipe(gulp.dest(config.assets.vendor)) ;
+	.pipe(gulp.dest(config.assets.vendor));
 });
 
-gulp.task('css', ['bower'], function() { 
+gulp.task('css', ['bower'], function() {
 	return gulp.src(config.assets.less + '/screen.less')
 	.pipe(less({
 		paths: [
 			config.assets.less,
 			config.assets.vendor + '/bootstrap/less',
 		]
-	}).on("error", notify.onError(function (error) {
+	}).on("error", notify.onError(function(error) {
 		console.log(error);
 		return "Error: " + error.message;
 	})))
 	.pipe(csso())
 	.pipe(autoprefix('last 2 versions'))
-	.pipe(gulp.dest(config.public.css)); 
+	.pipe(gulp.dest(config.public.css));
 });
 
-gulp.task('javascript', ['bower'], function(){  
+gulp.task('javascript', ['bower'], function() {
 	return gulp.src([
 		config.assets.vendor + '/jquery/dist/jquery.js',
 		config.assets.vendor + '/bootstrap/dist/js/bootstrap.js',
@@ -57,9 +57,9 @@ gulp.task('javascript', ['bower'], function(){
 	.pipe(gulp.dest(config.public.javascript));
 });
 
- gulp.task('watch', function() {
-	gulp.watch(config.assets.less + '/**/*.less', ['css']); 
-	gulp.watch(config.assets.javascript + '/**/*.js', ['javascript']); 
+gulp.task('watch', function() {
+	gulp.watch(config.assets.less + '/**/*.less', ['css']);
+	gulp.watch(config.assets.javascript + '/**/*.js', ['javascript']);
 });
 
-  gulp.task('default', ['bower', 'css', 'javascript']);
+gulp.task('default', ['bower', 'css', 'javascript']);
