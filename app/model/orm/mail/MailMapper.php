f2c28af3b329577d94c6feac17f438349eef6220
Jan Tojnar <jtojnar@gmail.com>
2016-10-25 21:08:32 +0200

    Remove mappings from MailMapper
    Nextras\Orm 2.1.0 shows an error when adding a mapping to a reflection
    and mapping for the key already exists. These mappings were introduced
    in the past because the library required them or something. Now it does
    not need them so we can just remove them.
    
    See: https://github.com/nextras/orm/issues/174

--- app/model/orm/mail/MailMapper.php
+++ app/model/orm/mail/MailMapper.php
@@ -3,11 +3,4 @@
 namespace App\Model;
 
 class MailMapper extends BaseMapper {
-	protected function createStorageReflection() {
-		$reflection = parent::createStorageReflection();
-		$reflection->addMapping('reaction', 'reaction');
-		$reflection->addMapping('recipient', 'recipient');
-		$reflection->addMapping('sender', 'sender');
-		return $reflection;
-	}
 }
