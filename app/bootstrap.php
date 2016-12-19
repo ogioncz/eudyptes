8f268d2f4c66d8109b82d7521151fdd0d1d10335
Jan Tojnar <jtojnar@gmail.com>
2016-12-19 21:23:19 +0100

    Use composer for autoloading

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -12,11 +12,6 @@ $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
 
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->addDirectory(__DIR__ . '/../vendor/others')
-	->register();
-
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 
