4dc170f6053956528aa2e9e1ede14cfa1aadf5ef
Jan Tojnar <jtojnar@gmail.com>
2022-05-31 14:21:28 +0200

    Remove remnants of robotloader
    We removed it in 8f268d2f4c66d8109b82d7521151fdd0d1d10335 from the app but it remained in tests.

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -9,10 +9,6 @@ Tester\Environment::setup();
 $configurator = new \Nette\Bootstrap\Configurator;
 $configurator->setDebugMode(false);
 $configurator->setTempDirectory(__DIR__ . '/../../temp');
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__ . '/../../app')
-	->addDirectory(__DIR__ . '/../../vendor/others')
-	->register();
 
 $configurator->addConfig(__DIR__ . '/../../app/config/config.neon');
 $configurator->addConfig(__DIR__ . '/../../app/config/config.local.neon');
